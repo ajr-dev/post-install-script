f02230734027c0e9df141eb3a732b09645b448ce
ajr-dev <arturojosejr@gmail.com>
2020-08-17 21:44:26 +0200

    Fixed order of font installation

--- install/settings/source-code-pro.sh
+++ install/settings/source-code-pro.sh
@@ -3,6 +3,30 @@
 CURRENT_DIR=$(dirname "$0")
 declare -f assertConfirmation &>/dev/null ||  source "$CURRENT_DIR/../declarations.sh"
 
+# Install patched nerd font
+if have_sudo_access; then
+  echo "Installing system wide"
+  FONTS_PATH="/usr/share/fonts/.local/share/fonts/"
+  sudo mkdir -p "$FONTS_PATH"
+  SauceCodePro="https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro"
+  sudo curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Regular.ttf" "$SauceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf"
+  sudo curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Bold.ttf" "$SauceCodePro/Bold/complete/Sauce%20Code%20Pro%20Bold%20Nerd%20Font%20Complete.ttf"
+  sudo curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Italic.ttf" "$SauceCodePro/Italic/complete/Sauce%20Code%20Pro%20Italic%20Nerd%20Font%20Complete.ttf"
+else
+  echo "Installing font for local user"
+  FONTS_PATH="$HOME/.local/share/fonts/"
+  mkdir -p "$FONTS_PATH"
+  SauceCodePro="https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro"
+  curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Regular.ttf" "$SauceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf"
+  curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Bold.ttf" "$SauceCodePro/Bold/complete/Sauce%20Code%20Pro%20Bold%20Nerd%20Font%20Complete.ttf"
+  curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Italic.ttf" "$SauceCodePro/Italic/complete/Sauce%20Code%20Pro%20Italic%20Nerd%20Font%20Complete.ttf"
+fi
+fc-cache -fv "$FONTS_PATH"  # update the font cache
+
+if fc-list | grep -q "Sauce"; then
+   exit # Installed scessflly:
+fi
+
 SYSTEM_FONTS=/usr/local/share/fonts
 HOME_FONTS=~/.local/share/fonts
 
@@ -20,20 +44,4 @@ else
   (git clone --branch release --depth 1 'https://github.com/adobe-fonts/source-code-pro.git' \
     "$HOME_FONTS/adobe-fonts/source-code-pro" && \
     fc-cache -fv "$HOME_FONTS/adobe-fonts/source-code-pro")  # update the font cache
-fi
-# TODO: install Source Code Pro from Nerd Fonts instead
-if have_sudo_access; then
-  FONTS_PATH="/usr/share/fonts/.local/share/fonts/"
-  sudo mkdir -p "$FONTS_PATH"
-  SauceCodePro="https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro"
-  sudo curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Regular.ttf" "$SauceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf"
-  sudo curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Bold.ttf" "$SauceCodePro/Bold/complete/Sauce%20Code%20Pro%20Bold%20Nerd%20Font%20Complete.ttf"
-  sudo curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Italic.ttf" "$SauceCodePro/Italic/complete/Sauce%20Code%20Pro%20Italic%20Nerd%20Font%20Complete.ttf"
-else
-  FONTS_PATH="$HOME/.local/share/fonts/"
-  mkdir -p "$FONTS_PATH"
-  SauceCodePro="https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro"
-  curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Regular.ttf" "$SauceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf"
-  curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Bold.ttf" "$SauceCodePro/Bold/complete/Sauce%20Code%20Pro%20Bold%20Nerd%20Font%20Complete.ttf"
-  curl -L -o "$FONTS_PATH/Sauce Code Pro Nerd Font Italic.ttf" "$SauceCodePro/Italic/complete/Sauce%20Code%20Pro%20Italic%20Nerd%20Font%20Complete.ttf"
 fi
